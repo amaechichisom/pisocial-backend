184fc86d5962e7bdf8195dfa4ad29ad941142ca3
Amaechi Chisom <amaechilegend@gmail.com>
2021-10-10 15:53:16 +0100

    Made changes

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://pisocial.netlify.app');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
