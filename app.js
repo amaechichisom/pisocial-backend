1741ddef394df8580898e17d7fd11fb453da5d4c
Amaechi Chisom <amaechilegend@gmail.com>
2021-10-10 17:53:34 +0100

    Made changes

--- app.js
+++ app.js
@@ -18,6 +18,20 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 app.options('*', cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+   
+  next();
+});
 // app.use(cors())
 
 const store = new MongoDBStore({
@@ -62,20 +76,7 @@ app.use(
 );
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-  );
-   
-  next();
-});
+
 
 
 
